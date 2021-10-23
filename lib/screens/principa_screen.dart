70f3d5a44ac17974805957deae08acbb2181a4d2
dnlknno <danielcardona180478@correo.itm.edu.co>
2021-10-23 17:17:45 -0500

    Boton visualizar animes

--- lib/screens/principa_screen.dart
+++ lib/screens/principa_screen.dart
@@ -12,8 +12,46 @@ class _PrincipalScreenState extends State<PrincipalScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('Principal Screen')
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _showLogo(),
+            SizedBox(height: 20,),
+            _showButtonAnimes(),
+          ],
+        )
         ),
+    );
+  }
+
+  Widget _showLogo() {
+    return Image(
+      image: AssetImage('assets/Animes.jpg'),
+      width: 380,
+    );
+  }
+
+  Widget _showButtonAnimes() {
+    return Container(
+      margin: EdgeInsets.only(left: 10, right: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child: ElevatedButton(
+              child: Text('Visualizar Animes'),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                  (Set<MaterialState> states) {
+                    return Colors.green;
+                  }
+                ),
+              ),
+              onPressed: () {},
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
